1d37264c6ce1aaaf83ae4b82b2a18495f9553264
Matt Mohan <matt.mohan@wishabi.com>
2016-03-16 17:33:11 -0400

    Add test to force a reek output

--- test.rb
+++ test.rb
@@ -1,3 +1,6 @@
 puts 'Hello World'
 puts "hollow world"
-puts "More Badly formatted"+  "Code to flag"  +"rubocop Warnings"
+puts "More Badly formatted" +  "Code to flag"  + "rubocop Warnings"
+def test_func (test_input)
+  puts "This is a really, really, long line that should cause additional warnings from tools like reek"
+end
