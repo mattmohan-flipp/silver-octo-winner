70048a72dd4c9a173882a8bae4adde0d44eb1508
Matt Mohan <matt.mohan@wishabi.com>
2016-03-17 13:05:11 -0400

    Add a second test case

--- test.rb
+++ test.rb
@@ -4,3 +4,10 @@ puts "More Badly formatted" +  "Code to flag"  + "rubocop Warnings"
 def test_func (test_input)
   puts "This is a really, really, long line that should cause additional warnings from tools like reek"
 end
+
+def test_func2 (test_input = false)
+  ar = [0, 1, 2, 3, 4]
+  ar.each do |ar_item|
+    puts ar_item if test_input
+  end
+end
