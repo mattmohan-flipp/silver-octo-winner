f8433f76de3343f692279717d5ffd42d99fd932a
Matt Mohan <matt.mohan@wishabi.com>
2016-04-05 17:19:05 -0400

    Additional test

--- test.rb
+++ test.rb
@@ -1,2 +1,3 @@
 puts 'Hello World'
 puts "hollow world"
+puts "A very long string that should trigger rubocop validation rules telling us to split the line somewhere. It's kinda silly, but it'll do the job of testing this behaviour"
